47e55cf0db8387a2b0a3359a37b315e2073bc864
Alexis Valencia <alex.valencia.06.av@gmail.com>
2022-09-20 15:22:48 -0700

    update example wrap

--- Example/Example/ExampleViewController.m
+++ Example/Example/ExampleViewController.m
@@ -62,7 +62,7 @@ NSString * const USER_ID_KEY = @"user_id";
 
 - (IBAction)sdkLoginButtonPressed:(UIButton *)sender {
     // Use the login page inside the SDK.  No credentials are necessary.
-    self.record360ViewController = [Record360ViewController loadControllerLoginAndSendTo:self.record360 displayOn:self showCancelButton:YES];
+    self.record360ViewController = [Record360ViewController loadControllerLoginAndSendTo:self.record360 displayOn:self showCancelButton:YES withLoginUsername:nil];
     self.record360ViewController.delegate = self;
     [self buildDefaultSettings];
 }
@@ -94,6 +94,7 @@ NSString * const USER_ID_KEY = @"user_id";
     NSLog(@"Error logging in %@.  Please check credentials and try again.", error);
 }
 
+
 - (void)updateSavedLogin {
     // Optionally, store the login credentials for future logins if that suits your needs.
     NSString *userToken = [[NSUserDefaults standardUserDefaults] objectForKey:USER_TOKEN_KEY];
@@ -105,14 +106,14 @@ NSString * const USER_ID_KEY = @"user_id";
 }
 
 // Notification immediately after a transaction has completed
-- (void)onTransactionComplete {
+- (void)onInspectionComplete {
     // Show progress dialog
     [self.record360 showProgressDialogOnViewController:self onControllerClose:nil];
     [self updateSavedLogin];
 }
 
 // Notification immediately after a transaction has cancelled
-- (void)onTransactionCanceled {
+- (void)onInspectionCanceled {
     [self showDialogTitle:@"Transaction Cancelled" andMessage:nil];
     [self updateSavedLogin];
 }
@@ -123,21 +124,21 @@ NSString * const USER_ID_KEY = @"user_id";
 }
 
 // Notification when a transaction has uploaded.  These come from the Record360 object.
-- (void)onTransactionUploadedForReferenceNumber:(NSString *)referenceNumber {
+- (void)onInspectionUploadedForReferenceNumber:(NSString *)referenceNumber {
     NSString *message = [NSString stringWithFormat:@"Transaction %@ uploaded successfully", referenceNumber];
     NSLog(@"%@", message);
     [self showDialogTitle:@"Transaction upload" andMessage:message]; // If the Record360ViewController or progress dialog is loaded and showing, then this alert will not show.
 }
 
 // Notification when a transaction has failed to upload.  These come from the Record360 object.
-- (void)onTransactionUploadFailedForReferenceNumber:(NSString *)referenceNumber withError:(NSError *)error {
+- (void)onInspectionUploadFailedForReferenceNumber:(NSString *)referenceNumber withError:(NSError *)error {
     NSString *message = [NSString stringWithFormat:@"An error occurred when uploading the transaction with reference number %@: %@", referenceNumber, error.localizedDescription];
     NSLog(@"%@", message);
     [self showDialogTitle:@"Transaction upload error" andMessage:message]; // If the Record360ViewController or progress dialog is loaded and showing, then this alert will not show.
 }
 
 // Notification when a transaction has stopped uploading due to the user deleting it.  These come from the Record360 object.
-- (void)onTransactionUploadDeletedForReferenceNumber:(NSString *)referenceNumber {
+- (void)onInspectionUploadDeletedForReferenceNumber:(NSString *)referenceNumber {
     NSLog(@"Transaction upload with reference number %@ was deleted.", referenceNumber);
 }
 
@@ -153,19 +154,19 @@ NSString * const USER_ID_KEY = @"user_id";
 
 - (void)buildDefaultSettings {
     [self.record360ViewController applyDefaultSettings:@[
-                                                         [[Record360Setting alloc] initSwitchSetting:SETTING_NOTATIONS_ON_IMAGES canDisplay:NO value:NO],
-                                                         [[Record360Setting alloc] initSwitchSetting:SETTING_VIN_SCAN canDisplay:YES value:NO],
-                                                         [[Record360Setting alloc] initSwitchSetting:SETTING_NATIVE_PHOTO_MODE canDisplay:NO value:NO],
-                                                         [[Record360Setting alloc] initOptionSetting:SETTING_RESOLUTION canDisplay:YES value:RESOLUTION_HIGH],
-                                                         [[Record360Setting alloc] initOptionSetting:SETTING_UPLOAD_MODE canDisplay:YES value:UPLOAD_MODE_ONLINE],
-                                                         [[Record360Setting alloc] initSetting:SETTING_SEND_SUPPORT_LOG],
-                                                         [[Record360Setting alloc] initSetting:SETTING_LOGOUT],
-                                                         [[Record360Setting alloc] initSetting:SETTING_LINKS label:@"Access Records" link:@"https://app.record360.com"],
-                                                         [[Record360Setting alloc] initSetting:SETTING_RATE_RECORD360],
-                                                         [[Record360Setting alloc] initSetting:SETTING_LINKS label:@"Terms of Service" link:@"https://app.record360.com/terms.html"],
-                                                         [[Record360Setting alloc] initSetting:SETTING_LINKS label:@"Privacy" link:@"https://app.record360.com/privacy.html"],
-                                                         [[Record360Setting alloc] initSetting:SETTING_VERSION]
-                                                         ]];
+        [[Record360Setting alloc] initSwitchSetting:SETTING_NOTATIONS_ON_IMAGES canDisplay:NO value:NO],
+        [[Record360Setting alloc] initSwitchSetting:SETTING_VIN_SCAN canDisplay:YES value:NO],
+        [[Record360Setting alloc] initSwitchSetting:SETTING_NATIVE_PHOTO_MODE canDisplay:NO value:NO],
+        [[Record360Setting alloc] initOptionSetting:SETTING_RESOLUTION canDisplay:YES value:RESOLUTION_HIGH],
+        [[Record360Setting alloc] initOptionSetting:SETTING_UPLOAD_MODE canDisplay:YES value:UPLOAD_MODE_ONLINE],
+        [[Record360Setting alloc] initSetting:SETTING_SEND_SUPPORT_LOG],
+        [[Record360Setting alloc] initSetting:SETTING_LOGOUT],
+        [[Record360Setting alloc] initSetting:SETTING_LINKS label:@"Access Records" link:@"https://app.record360.com"],
+        [[Record360Setting alloc] initSetting:SETTING_RATE_RECORD360],
+        [[Record360Setting alloc] initSetting:SETTING_LINKS label:@"Terms of Service" link:@"https://app.record360.com/terms.html"],
+        [[Record360Setting alloc] initSetting:SETTING_LINKS label:@"Privacy" link:@"https://app.record360.com/privacy.html"],
+        [[Record360Setting alloc] initSetting:SETTING_VERSION]
+    ]];
 }
 
 @end
